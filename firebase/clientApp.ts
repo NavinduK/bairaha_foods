72905d37da2a5e6a2ff53878316be41fce65a310
Navindu Kavishka <navindu.nk@gmail.com>
2023-12-10 20:22:16 +0530

    environment variables

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCWCpc79AyLLFA3aToVw-oXhMzG90zOLoo",
-  authDomain: "bairahafoods-2192c.firebaseapp.com",
-  projectId: "bairahafoods-2192c",
-  storageBucket: "bairahafoods-2192c.appspot.com",
-  messagingSenderId: "1073862361808",
-  appId: "1:1073862361808:web:1d56bec66494e07ef514d5",
-  measurementId: "G-SP4BCWEP2F",
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
 
 // Initialize Firebase
--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  env: { NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_APIKEY: process.env.NEXT_PUBLIC_APIKEY,
+    NEXT_PUBLIC_AUTHDOMAIN: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+    NEXT_PUBLIC_PROJECTID: process.env.NEXT_PUBLIC_PROJECTID,
+    NEXT_PUBLIC_STORAGEBUCKET: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+    NEXT_PUBLIC_MESSAGINGSENDERID: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+    NEXT_PUBLIC_APPID: process.env.NEXT_PUBLIC_APPID,
+    NEXT_PUBLIC_MEASUREMENTID: process.env.NEXT_PUBLIC_MEASUREMENTID,
+  },
 }
 
 module.exports = nextConfig
